064af7f3c3e94db1e5e0a4e82cc76fd3734b252b
iii <miselrkba@gmail.com>
2021-01-18 23:59:23 +0100

    insert 1 record

--- mongoDb.js
+++ mongoDb.js
@@ -1,12 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = 'mongodb://localhost:27017/';
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  const dbo = db.db("mydb");
-  dbo.createCollection("customers", function(err, res) {
+  const dbo = db.db('mydb');
+  const myObj = { name: 'My Company', address: 'Rk', country: 'Slovakia' };
+
+  // dbo.createCollection("customers", function(err, res) {
+  //   if (err) throw err;
+  //   console.log("Collection created!");
+  //   db.close();
+  // });
+
+  dbo.collection('customers').insertOne(myObj, function (err, res) {
     if (err) throw err;
-    console.log("Collection created!");
+    console.log('1 doc inserted');
     db.close();
   });
 });
